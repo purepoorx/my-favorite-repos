cdd00651fe9cf93238b962bfe1ee3efaf8f916cd
purepoorx <purepoorx@users.noreply.github.com>
2024-09-12 16:32:32 +0000

    Apply automatic changes

---
+++ github.com_Cl0udG0d_Fofa-hack_main/docs/CHANGELOG.md
@@ -0,0 +1,330 @@
+# CHANGELOG 代码变更记录
+
+### 2.5.1
++ 添加 --fofa-key 参数 (优化中)
++ 修复BUG [issue 86](https://github.com/Cl0udG0d/Fofa-hack/issues/86)
+
+### 2.4.18
++ 合入(pull request)(https://github.com/Cl0udG0d/Fofa-hack/pull/85)
+
+### 2.4.17
++ 删除文档里面的存在问题的部分
++ 修改setup.py，更新fofa-hack包为0.02版本 [pip fofa-hack](https://pypi.org/manage/project/fofa-hack/release/0.0.2/)
+
+### 2.4.16
++ 新增 --authorization-file 参数
+
+### 2.4.15
++ 添加批量从文件使用代理的功能 --proxy-file
++ 添加使用网址代理的功能 --proxy-url
++ 在使用AUTHORIZATION的情况下,一次获取50条数据
+
+### 2.4.14
++ 修改并测试了代理的问题
+
+### 2.4.13
++ 新增icon文件和网址功能 [issue 64](https://github.com/Cl0udG0d/Fofa-hack/issues/64)
++ 修复BUG [issue 65](https://github.com/Cl0udG0d/Fofa-hack/issues/65)
+
+### 2.4.12
++ 修复[issue 63](https://github.com/Cl0udG0d/Fofa-hack/issues/63)
++ 完善Authorization逻辑
+
+### 2.4.11
++ 新增SECURITY文件
++ 新增用户Authorization查询
+
+### 2.4.10
++ 修复删除文件问题
++ 修复[issue 59](https://github.com/Cl0udG0d/Fofa-hack/issues/59)
+
+### 2.4.9
++ 修复语言问题
++ 修改代码冗余部分
+
+### 2.4.8
++ 修复setup.py问题
++ 合并[新增可设置保存文件名代码](https://github.com/Cl0udG0d/Fofa-hack/pull/58)
++ 新增贡献者[10cks](https://github.com/10cks)
+
+### 2.4.7
++ 去除没有获取到语言情况下为空的问题
++ 生成fofa-hack pip包
+
+### 2.4.6
++ 修复 [issues 54](https://github.com/Cl0udG0d/Fofa-hack/issues/54)
++ 终端颜色输出问题解决
+
+### 2.4.5
++ 修复 [issues 51](https://github.com/Cl0udG0d/Fofa-hack/issues/51)
++ 修复 [issues 53](https://github.com/Cl0udG0d/Fofa-hack/issues/53)
+
+### 2.4.4
++ unit.py 修剪关键字功能扩展
+
+### 2.4.3
+
++ 修复打包问题
+
+### 2.4.2
+
++ 修复打包问题
+### 2.4.1
+
++ 根据环境切换语言
+
+### 2.4.0
+
++ 删除多余逻辑文件
++ 更新获取数据逻辑,页面爬虫 -> api接口获取
++ 默认步长设置为 50,防止等待过久
+### 2.3.9
+
++ 修复exit(0)退出报错问题
++ 无时间戳的情况下退出并保存final文件
+
+### 2.3.8
+
++ 输出内容为json的情况下 ,level=2、3时报错问题修复
+
+### 2.3.7
+
++ 添加地区访问fofa判断
++ linux环境下不支持运行selenium模式
+
+### 2.3.6
+
++ 修复[ISSUE](https://github.com/Cl0udG0d/Fofa-hack/issues/34)
+
+### 2.3.5
+
++ 修复[ISSUE](https://github.com/Cl0udG0d/Fofa-hack/issues/22)
++ 高亮界面输出
++ 新增exe输出
+
+### 2.3.4
+
++ 新增英文readme
+### 2.3.3
+
++ 修复部分已知BUG 
+  + 修复搜索结果为0时出现的列表索引越界问题
+  + 修复搜索结果时间戳列表为空时的报错问题
+### 2.3.2
+
++ 修改运行方式readme
+
+### 2.3.1
+
++ 新增selenium部分
++ 新增type字段 虽然它不太稳定
+
+### 2.3.0
+
++ 分离参数输入和爬取部分
+
+### 2.2.6
+
++ 新增fofa公安版登录代码,该部分未完善
+
+### 2.2.5
+
++ 从文件批量读取语法 -i --inputfile
+
+### 2.2.4
+
++ 修复变量名BUG
+
+### 2.2.3
+
++ 除特殊变量外,变量统一命名规范为 小写字母+下划线
++ 除特殊方法外,方法一律使用小驼峰命名 camelCase
++ 方法名见名知意 复杂方法添加注释
++ 代码格式化
+
+### 2.2.2
+
++ 增加外部调用接口
++ 递归结束使用全局变量判定
+
+### 2.2.1
+
++ 合并tastypear程序兼容性功能
+
+### 2.2.0
+
++ 合并wanswu代理功能
+
+### 2.1.12
+
++ 支持JSON文件输出 -o json
+
+### 2.1.11
+
++ 修订 python maximum recursion depth exceeded错误
++ json文件输出BUG修改
+
+### 2.1.10
+
++ 修改logo
+
+### 2.1.9
+
++ 修改readme对于保存文件的描述
++ 实现fofa的两种URL获取逻辑，增加获取内容
+
+### 2.1.8
+
++ 增加了fuzz之后的去重 免得获取到一堆重复数据
+
+### 2.1.7
+
++ 新增fuzz参数 -f 开启
++ 修复header问题
+
+### 2.1.6
+
++ 删除测试 print
++ 新增 != fuzz 逻辑
+
+### 2.1.5
+
++ 删除config.py文件,合并内容到fofa.py
++ 每次递归的时候的关键字列表应当与下标对应，而不是统一SET
++ 运行测试 python fofa.py -k index --endcount 1000
++ 合并FUZZ关键字并进行递归
+
+### 2.1.4
+
++ 修复 https://github.com/Cl0udG0d/Fofa-hack/issues/13 ISSUE BUG
+
+### 2.1.3
+
++ 添加 port 自动化fuzz
++ 新增 搜索关键字 port判断
+
+### 2.1.2
+
++ 修复 host="edu.cn" 搜索错误
+
+### 2.1.1
+
++ 提升程序稳定性
++ 添加org 、asn 自动化fuzz
++ 修改递归逻辑
++ 新增项目头图
++ 新增TODO文件
+
+### 2.1.0
+
++ 删除多余images
++ 修改readme图片
++ 添加country 自动化fuzz
+
+### 2.0.6
+
++ 修改README等文件
++ 合并代码 - 优化时间戳逻辑
+
+### 2.0.5
++ 新增贡献者 tastypear
++ 尝试优化时间戳逻辑
+
+### 2.0.4
++ 删除.github文件夹
++ 新增TODO List
++ 删除csv导出
+
+### 2.0.3
++ 修复fofa单关键字下第二页错误情况
+
+### 2.0.2
++ 修复fofa多个查询条件下出现的错误
+
+### 2.0.1
++ 修改filename,timeSleep等为类内部变量
++ 新增logoutInitMsg方法输出初始化信息
++ 新增fofa_fuzz_spider方法
+
+### 2.0.0
+
++ 返璞归真,去除账号登录,直接使用时间戳来在第一页进行爬取
+
+### 1.3.1
+
++ 支持多种导出方式 txt,json,csv
++ 因FOFA对普通用户的限制停止该项目的更新
+### 1.3.0
+
++ `README`添加示例运行参考
++ 新增工具文件夹`tookit`,创建`levelData`等级模板文件
++ 新增 `--level`参数,内容明细如下
+  + level=1 [ url ]
+  + level=2 [ url , port , title , ip ]
+  + level=3 [ url , port , title , ip , city , asn , organization , server , rep ]
+
+### 1.2.8
+
++ 修改keyword为必须参数
+
+### 1.2.7
+
++ 添加运行参数模式 --help
++ 修改README.md等文件
++ 支持三种方式登录账号
+
+### 1.2.6
+
++ 设置开始页码为1,不支持自定义开始页码,防止普通用户无法查看页面问题
++ 添加赞赏账户文档与二维码
++ 修改README
+
+### 1.2.5
+
++ 修改`ddddocr`版本号
+
+### 1.2.4
+
++ 修改切换账号逻辑,首先重试当前账号
+
+### 1.2.3
+
++ 显示运行过程中的报错详情
++ 优化项目结构
++ 支持复制cookie和账号密码两种方式登录,防止ddddocr等库环境问题
++ 新增工具文件`unit.py`
+
+### 1.2.2
+
++ BUG修复
+> 存储结果到`md5(搜索关键字)_运行时间戳.txt`文件中
+
+
+### 1.2.1
+
++ 添加`QUESTIONS.md`问题集合文件
++ 增加断点重连机制，重连最大次数为3,提高系统运行稳定性
++ 增加多账户
++ 部分文件的微小更新(fofa_useragent.py、README.md....)
+
+### 1.2.0
+
++ 添加CHANGELOG文件
++ 优化代码结构，分离useragent文件
++ 提取版本号在项目启动时加载
++ FOFA登录重试机制，最大重试值默认为3
++ 添加部分代码注释
++ 自动生成文件名 搜索关键字_运行时间戳.txt
+
+
+### 此前的一些提交
+
++ 2023-1-27，合入[wanswu](https://github.com/wanswu)师傅提交的代码，通过账号密码进行登录
+
++ 2022-12-19，添加cookie的预判断
+
++ 2022-11-12，更新爬取规则
+
++ 2022-9-17，修改时间戳获取规则，项目整体趋于稳定
+
++ 2022-9-15，创建项目并修改适用于目前fofa的规则
---
+++ github.com_Cl0udG0d_Fofa-hack_main/fofa.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/9/14 22:20
+# @Author  : Cl0udG0d
+# @File    : fofa.py
+# @Github: https://github.com/Cl0udG0d
+import argparse
+import os
+import sys
+import time
+from core.fofaMain import FofaMain
+from tookit import unit, config
+from tookit.levelData import LevelData
+from tookit.outputData import OutputData
+from tookit.unit import clipKeyWord, outputLogo
+import gettext
+import locale
+import mmh3
+import requests
+import base64
+import codecs
+config.ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+if getattr(sys, 'frozen', None):
+    dir = sys._MEIPASS
+else:
+    dir = config.ROOT_PATH
+# 获取当前的语言设置
+lang, _ = locale.getdefaultlocale()
+if lang and lang.startswith('zh'):
+    # 如果是中文环境，不需要翻译，直接用原始字符串
+    _ = lambda x: x
+else:
+    # 如果是其他语言环境，则加载对应的翻译文件
+    language = gettext.translation('fofa_hack', localedir=os.path.join(dir, "locale"), languages=['en'])
+    language.install()
+    _ = language.gettext
+
+
+def main():
+    outputLogo()
+    parser = argparse.ArgumentParser(description=_("Fofa-hack v{} 使用说明").format(config.VERSION_NUM))
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--keyword', '-k', help=_('fofa搜索关键字'))
+    group.add_argument('--inputfile', '-i', help=_("指定文件,从文件中批量读取fofa语法"))
+    group.add_argument('--base', '-b', help=_("以base64的形式输入关键字 -b InRoaW5rcGhwIg=="))
+    group.add_argument('--iconurl', type=str, help="指定url的icon作为icon_hash关键字")
+    group.add_argument('--iconfile', type=str, help="指定icon_file作为icon_hash关键字")
+
+    parser.add_argument('--timesleep', '-t', help=_('爬取每一页等待秒数,防止IP被Ban,默认为3'), default=3)
+    parser.add_argument('--timeout', '-to', help=_('爬取每一页的超时时间,默认为180秒'), default=180)
+    parser.add_argument('--endcount', '-e', help=_('爬取结束数量'))
+    parser.add_argument('--level', '-l', help=_('爬取等级: 1-3 ,数字越大内容越详细,默认为 1'))
+    parser.add_argument('--output', '-o', help=_('输出格式:txt、json,默认为txt'))
+    parser.add_argument('--outputname','-on', help=_("指定输出文件名，默认文件名为 fofaHack"))
+    parser.add_argument('--fuzz', '-f', help=_('关键字fuzz参数,增加内容获取粒度'), action='store_true')
+
+    parser.add_argument('--proxy-type',choices=['socks4','socks5', 'http'], help=_("代理类型,默认为http"),default='http')
+    parser.add_argument('--authorization', type=str, help="指定Authorization值")
+    parser.add_argument('--authorization-file', type=str, help="从文件中读取authorization列表 --authorization-file authorization.txt")
+
+    parser.add_argument('--fofa-key', type=str,
+                        help="fofa api key值(配合fofa终身会员使用)")
+
+    proxy_group = parser.add_mutually_exclusive_group()
+    proxy_group.add_argument('--proxy', help=_("指定代理,代理格式 --proxy '127.0.0.1:7890'"))
+    proxy_group.add_argument('--proxy-url', help=_("指定代理url，即访问URL响应为proxy,代理格式 --proxy-url http://127.0.0.1/proxy_pool/get"))
+    proxy_group.add_argument('--proxy-file', help=_("指定txt格式的代理文件,按行分割,代理格式 --proxy-file proxy.txt"))
+    # parser.add_argument('--type', type=str, choices=["common", "selenium"], default="common",
+    #                     help="运行类型,默认为普通方式")
+    args = parser.parse_args()
+
+    if args.fofa_key:
+        config.FOFA_KEY = args.fofa_key
+
+    if args.authorization_file:
+        config.AUTHORIZATION_FILE = args.authorization_file
+        with open(config.AUTHORIZATION_FILE, 'r') as f:
+            for line in f.readlines():
+                config.AUTHORIZATION_LIST.append(line)
+
+    time_sleep = int(args.timesleep)
+    timeout = int(args.timeout)
+    if args.keyword:
+        search_key = clipKeyWord(args.keyword)
+    elif args.base:
+        try:
+            search_key = base64.b64decode(args.base).decode('utf-8')
+        except Exception as e:
+            print(e)
+            search_key = ""
+            pass
+    elif args.iconurl:
+        if args.iconurl.endswith(".ico"):
+            icon_url = args.iconurl
+        else :
+            icon_url = args.iconurl + "favicon32.ico" if  args.iconurl.endswith("/") else args.iconurl + "/favicon32.ico"
+        try:
+            _icon = mmh3.hash(
+                codecs.lookup('base64').encode(requests.get(icon_url).content)[0])
+            search_key = '''icon_hash="{}"'''.format(_icon)
+        except:
+            print("icon url " + icon_url + " 访问错误")
+            exit(0)
+    elif args.iconfile:
+        file_path = args.iconfile
+        if os.path.exists(file_path):
+            with open(file_path, 'rb') as f:
+                _icon = mmh3.hash(codecs.lookup('base64').encode(f.read())[0])
+                search_key = '''icon_hash="{}"'''.format(_icon)
+    else:
+        search_key = ""
+
+    endcount = int(args.endcount) if args.endcount else 100
+    level = args.level if args.level else "1"
+    level_data = LevelData(level)
+    fuzz = args.fuzz
+
+
+    output = args.output if args.output else "txt"
+    outputname = args.outputname if args.outputname else "fofaHack"
+
+    if search_key:
+        # if outputname:
+        filename="{}.{}".format(outputname,output)
+        # 检查文件是否存在
+        if os.path.exists(filename) and os.path.exists("final_"+filename):
+            # 如果存在，删除文件
+            os.remove(filename)
+            os.remove("final_"+filename)
+        # else:
+        #     filename = "{}_{}.{}".format(unit.md5(search_key), int(time.time()), output)
+        output_data = OutputData(filename, level, pattern=output)
+    else:
+        filename = _("暂无")
+        output_data = None
+
+    if args.proxy or args.proxy_url or args.proxy_file :
+        config.IS_PROXY = True
+        if args.proxy:
+            config.PROXY_SINGLE = True
+            config.PROXY_ARGS = args.proxy
+        elif args.proxy_url :
+            config.PROXY_FROM_URL = True
+            config.PROXY_ARGS = args.proxy_url
+        else:
+            config.PROXY_FROM_TXT = True
+            config.PROXY_ARGS = args.proxy_file
+    if args.proxy_type:
+        config.PROXY_TYPE = args.proxy_type
+
+    # is_proxy, proxy = setProxy(args.proxy,args.proxy_type)
+    # type = args.type
+    inputfile = args.inputfile if args.inputfile else None
+    if not inputfile and not search_key:
+        print(_("未输入搜索内容"))
+        exit(0)
+    if args.authorization:
+        # 用户输入了Authorization值
+        # print("用户输入的Authorization值为:", args.authorization)
+        config.AUTHORIZATION = args.authorization
+
+    fofa = FofaMain(search_key, inputfile, filename, time_sleep, endcount, level, level_data, output, output_data,
+                    fuzz, timeout, config.IS_PROXY)
+    fofa.start()
+
+
+if __name__ == '__main__':
+    main()
