9d7029b5a4a407be3f1493233c81ced063b0e7b2
purepoorx <107200810+purepoorx@users.noreply.github.com>
2025-05-25 01:02:36 +0000

    Apply automatic changes

--- gitlab.com_ivgeek_mixfile_master/app/src/main/java/com/donut/mixfile/ui/component/MainPage.kt
+++
@@ -1,9 +0,0 @@
-package com.donut.mixfile.ui.component
-
-import androidx.compose.runtime.Composable
-import com.donut.mixfile.ui.nav.NavComponent
-
-@Composable
-fun MainContent() {
-    NavComponent()
-}
